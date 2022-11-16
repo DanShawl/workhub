426d1b11d33d38e66f98fa85e6f28db90b0f7539
Dan Shawl <danielrshawl@gmail.com>
2022-11-15 23:59:18 -0500

    Added equipmentModel

---
+++ backend/models/equipmentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const equipmentSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description value'],
+    },
+    status: {
+      type: String,
+      required: [true, 'Please add a status'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Equipment', equipmentSchema);
