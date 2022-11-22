1f6508783d90c6fa100378b466c641426460ace4
Dan Shawl <danielrshawl@gmail.com>
2022-11-21 23:23:57 -0500

    added tw styling for header/side nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,42 +2,102 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../features/auth/authSlice';
-
+import { useState } from 'react';
+import {
+  BiMenu,
+  BiX,
+  BiTask,
+  BiCalendar,
+  BiBuildings,
+  BiUserCircle,
+} from 'react-icons/bi';
 function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const { user } = useSelector((state) => state.auth);
-
+  const [toggleNav, setToggleNav] = useState(false);
   const onLogout = () => {
     dispatch(logout());
     dispatch(reset());
     navigate('/');
   };
 
+  //  set up state for open and close
+
   return (
-    <header className="header">
-      <div className="logo">
+    <header className="bg-white md:h-screen w-screen md:w-60 fixed top-0 left-0 md:border-r-2 border-zinc-200">
+      <div
+        className={`flex items-center h-14 p-3 justify-between ${
+          !toggleNav && 'border-b border-zinc-200 md:border-0'
+        }`}
+      >
         <Link to="/">WorkHub</Link>
-      </div>
-      <ul>
-        {user ? (
-          <li className="task-button">
-            <button className="" onClick={onLogout}>
-              Logout
-            </button>
-          </li>
+        {toggleNav ? (
+          <button
+            className="md:hidden"
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <BiX className="text-2xl" />
+          </button>
         ) : (
+          <button
+            className="md:hidden"
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <BiMenu className="text-2xl" />
+          </button>
+        )}
+      </div>
+      <nav className=" bg-white fixed top-14 right-0 w-full px-3 md:w-60 md:left-0 border-r-2 border-zinc-200">
+        {user ? (
           <>
+            <ul
+              className={
+                (toggleNav ? 'left-0 flex-1' : '-left-full ') +
+                ' transition-left fixed bottom-0 top-14 w-full items-center space-y-3  bg-white px-5 pt-8 font-semibold leading-3 text-black duration-500 sm:pt-12  md:static md:w-auto md:space-y-2  md:px-0 lg:pt-8 md:bg-white'
+              }
+            >
+              <li className="">
+                <button className="flex items-center gap-3 hover:bg-zinc-100 w-full rounded-sm md:px-3">
+                  <BiTask className="text-2xl" /> Facility Tasks
+                </button>
+              </li>
+              <li className="">
+                <button className="flex items-center gap-3 hover:bg-zinc-100 w-full rounded-sm md:px-3">
+                  <BiBuildings className="text-2xl" /> Work Orders
+                </button>
+              </li>
+              <li className="">
+                <button className="flex items-center gap-3 hover:bg-zinc-100 w-full rounded-sm md:px-3">
+                  <BiCalendar className="text-2xl" /> Schedule
+                </button>
+              </li>
+              <li className="">
+                <button className="flex items-center gap-3 hover:bg-zinc-100 w-full rounded-sm md:px-3">
+                  <BiUserCircle className="text-2xl" /> Contacts
+                </button>
+              </li>
+              <li className="">
+                <button
+                  className="w-full bg-zinc-200 rounded mt-6"
+                  onClick={onLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <ul>
             <li>
               <Link to="/login">Login</Link>
             </li>
             <li>
               <Link to="/register">Register</Link>
             </li>
-          </>
+          </ul>
         )}
-      </ul>
+      </nav>
     </header>
   );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,20 +6,7 @@ import Spinner from '../components/Spinner';
 import { getTasks, reset } from '../features/tasks/taskSlice';
 import TaskItem from '../components/TaskItem';
 import CategoryCount from '../components/CategoryCount';
-import {
-  BiBuildings,
-  BiCopyAlt,
-  BiListCheck,
-  BiPackage,
-  BiPlus,
-  BiReceipt,
-  BiRightArrowAlt,
-  BiSpreadsheet,
-  BiTask,
-  BiTimeFive,
-  BiTrash,
-  BiWrench,
-} from 'react-icons/bi';
+import { BiPackage, BiTask, BiWrench } from 'react-icons/bi';
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -55,65 +42,67 @@ function Dashboard() {
 
   return (
     <>
-      <section className="heading">
-        <h1>Welcome {user && user.name}</h1>
-        <section className="category-counters">
-          <CategoryCount tasks={tasks} />
+      <section className="md:ml-60 px-4">
+        <section className="heading">
+          <h1>Welcome {user && user.name}</h1>
+          <section className="category-counters">
+            <CategoryCount tasks={tasks} />
+          </section>
         </section>
-      </section>
-      <TaskForm />
-      <section className="categories-section">
-        <h2>Current Work</h2>
+        <TaskForm />
+        <section className="categories-section">
+          <h2>Current Work</h2>
 
-        <div className="categories">
-          <button
-            className={`btn-reverse ${category === 'All' ? 'active' : ''} `}
-            onClick={() => setCategory('All')}
-          >
-            All Work
-          </button>
-          <button
-            className={`btn-reverse ${
-              category === 'Work Order' ? 'active' : ''
-            } `}
-            onClick={() => setCategory('Work Order')}
-          >
-            <BiWrench />
-            Work Orders
-          </button>
-          <button
-            className={`btn-reverse ${category === 'Task' ? 'active' : ''} `}
-            onClick={() => setCategory('Task')}
-          >
-            <BiTask />
-            Facility Tasks
-          </button>
-          <button
-            className={`btn-reverse ${
-              category === 'Equipment Report' ? 'active' : ''
-            } `}
-            onClick={() => setCategory('Equipment Report')}
-          >
-            <BiPackage />
-            Equipment Reports
-          </button>
-        </div>
-      </section>
-      <section className="content">
-        {tasks.length > 0 ? (
-          <div className="goals">
-            {category !== 'All'
-              ? tasks
-                  .filter((task) => task.category === category)
-                  .map((task) => <TaskItem key={task._id} task={task} />)
-                  .reverse()
-              : tasks
-                  .map((task) => <TaskItem key={task._id} task={task} />)
-                  .reverse()}
+          <div className="categories">
+            <button
+              className={`btn-reverse ${category === 'All' ? 'active' : ''} `}
+              onClick={() => setCategory('All')}
+            >
+              All Work
+            </button>
+            <button
+              className={`btn-reverse ${
+                category === 'Work Order' ? 'active' : ''
+              } `}
+              onClick={() => setCategory('Work Order')}
+            >
+              <BiWrench />
+              Work Orders
+            </button>
+            <button
+              className={`btn-reverse ${category === 'Task' ? 'active' : ''} `}
+              onClick={() => setCategory('Task')}
+            >
+              <BiTask />
+              Facility Tasks
+            </button>
+            <button
+              className={`btn-reverse ${
+                category === 'Equipment Report' ? 'active' : ''
+              } `}
+              onClick={() => setCategory('Equipment Report')}
+            >
+              <BiPackage />
+              Equipment Reports
+            </button>
           </div>
-        ) : (
-          <h3>You have not set any Tasks.</h3>
-        )}
+        </section>
+        <section className="content">
+          {tasks.length > 0 ? (
+            <div className="goals">
+              {category !== 'All'
+                ? tasks
+                    .filter((task) => task.category === category)
+                    .map((task) => <TaskItem key={task._id} task={task} />)
+                    .reverse()
+                : tasks
+                    .map((task) => <TaskItem key={task._id} task={task} />)
+                    .reverse()}
+            </div>
+          ) : (
+            <h3>You have not set any Tasks.</h3>
+          )}
+        </section>
       </section>
     </>
   );
