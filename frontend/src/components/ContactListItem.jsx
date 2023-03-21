1f53800bc922814c98a9d214593a9002eec74ba0
Dan Shawl <danielrshawl@gmail.com>
2023-03-21 12:10:20 -0400

    UI changes to mobile contacts and font. Will be updating routing for individual contacts next

--- frontend/src/components/ContactListItem.jsx
+++ frontend/src/components/ContactListItem.jsx
@@ -3,8 +3,11 @@ import { CiMail, CiPhone } from 'react-icons/ci';
 
 const ContactListItem = ({ contact }) => {
   return (
-    <li className="grid grid-cols-2 gap-y-4 sm:gap-y-0 sm:grid-cols-5 w-full px-6 py-4 gap-x-6 border-[1px] border-b-0 border-gray-200 hover:cursor-pointer hover:bg-[#f2f2f2]">
-      <div className="flex items-center gap-x-2">
+    <li
+      className="grid grid-cols-2 gap-y-4 sm:gap-y-0 sm:grid-cols-5 w-full px-6 py-4 gap-x-6 border-[1px] border-b-0 border-gray-200 hover:cursor-pointer hover:bg-[#f2f2f2] font-sans"
+      onClick={() => console.log(contact._id)}
+    >
+      <div className="flex items-center gap-x-2 col-span-2 sm:col-span-1">
         <div className=" bg-gray-200 rounded-full w-10 h-10 flex justify-center items-center">
           {contact.firstName[0]}
           {contact.lastName[0]}
